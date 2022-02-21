9f6814b6cbe0cb2fe75b0eba33b5d969b0a21206
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-22 00:41:40 +0600

    stringified

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('hello');
+// JSON,Stringified
+
+const shop = {
+    name: 'Alia Shop',
+    address: 'Kaderabad Housing',
+    products: ['laptop', 'mobile', 'pepsi'],
+    isExpensive: false
+}
+const shopDetail = JSON.stringify(shop);
+console.log(shopDetail);
+// 
+const converted = JSON.parse(shopDetail);
+console.log(converted);
