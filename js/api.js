b4b2a9fda5658279990676a4dea938e6476e596f
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-22 18:56:52 +0600

    load data

--- js/api.js
+++ js/api.js
@@ -1,18 +1,37 @@
 function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(data => console.log(data));
 };
 
 
 function loadUser() {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => displayUser(data));
 }
 
-function loadPost(){
+function displayUser(data) {
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `name:${user.name}`;
+        ul.appendChild(li);
+    }
+}
+
+
+function loadPost() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>response.json())
-    .then(data=>console.log(data))
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function displayUser(data) {
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `name:${user.name} email:${user.email}`;
+        ul.appendChild(li);
+    }
 }
